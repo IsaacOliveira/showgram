5fc6dc30401f103347f3a5bbca34fe924b18111d
Isaac Oliveira <isaacoli@gmail.com>
2018-02-09 00:21:59 -0200

    move configuration from yml to app config

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,11 +1,10 @@
-CONFIG = YAML.load_file(Rails.root.join("config/app.yml"))[Rails.env]
 module APP_CONFIG
 
   module INSTAGRAM
-    APP_REDIRECT_URL = CONFIG['redirect_uri']
+    APP_REDIRECT_URL = ENV['REDIRECT_URL']
     GRANT_TYPE = 'authorization_code'
-    CLIENT_SECRET = CONFIG['client_secret']
-    CLIENT_ID = CONFIG['client_id']
+    CLIENT_SECRET = ENV['CLIENT_SECRET']
+    CLIENT_ID = ENV['CLIENT_ID']
     ACCESS_TOKEN_URL = "https://api.instagram.com/oauth/access_token"
     AUTHORIZE_URL = "https://www.instagram.com/oauth/authorize/?client_id=#{CLIENT_ID}&redirect_uri=#{APP_REDIRECT_URL}&response_type=code"
   end
